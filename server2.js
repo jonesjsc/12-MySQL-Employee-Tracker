e2ac942e9a7abcc1fc8555e8fae693fbaa832bef
Jason Jones <jason.e.jones@gmail.com>
2021-04-20 22:34:09 -0400

    abandoned async await and returned to inline.  All querys working

--- server2.js
+++ server2.js
@@ -2,6 +2,7 @@ const mysql = require("mysql");
 const inquirer = require("inquirer");
 const util = require("util");
 const cTable = require("console.table");
+const express = require("express");
 require("dotenv").config();
 
 const connection = mysql.createConnection({
@@ -120,20 +121,82 @@ const start = () => {
 };
 start();
 
-async function viewEmpByAll() {
-  await console.log("viewEmpByAll");
-  start();
+function viewEmpByAll() {
+  const query =
+    "SELECT e.id, e.first_name, e.last_name, r.title, d.name 'department', r.salary, concat(m.first_name, ' ',m.last_name) 'Manager' FROM employee e LEFT JOIN employee m ON (e.manager_id = m.id) JOIN role r on e.role_id=r.id JOIN department d on r.department_id=d.id ORDER BY e.id;";
+  connection.query(query, (err, res) => {
+    console.table(res);
+    start();
+  });
 }
 
 async function viewEmpByDept() {
-  await console.log("viewEmpByDept");
-  start();
+  const query = "SELECT name from department";
+  connection.query(query, (err, results) => {
+    if (err) throw err;
+    inquirer
+      .prompt([
+        {
+          name: "choice",
+          type: "list",
+          message: "Which Department?",
+          choices() {
+            const choiceArray = [];
+            results.forEach(({ name }) => {
+              choiceArray.push({ name });
+            });
+            // console.log(choiceArray);
+            return choiceArray;
+          },
+        },
+      ])
+      .then((answer) => {
+        connection.query(
+          "SELECT e.id, e.first_name, e.last_name, r.title, d.name 'department', r.salary, concat(m.first_name, ' ',m.last_name) 'Manager' FROM employee e LEFT JOIN employee m ON (e.manager_id = m.id) JOIN role r on e.role_id=r.id JOIN department d on r.department_id=d.id WHERE d.name = ? ORDER BY e.id;",
+          [answer.choice],
+          (err, res) => {
+            console.table(res);
+            start();
+          }
+        );
+      });
+  });
+  //   start();
 }
 
-async function viewEmpByMgr() {
-  await console.log("viewEmpByMgr");
-  start();
-}
+const viewEmpByMgr = () => {
+  // query the database for a list of all the departments
+  const query =
+    "SELECT DISTINCT e.id, concat(e.first_name, ' ',e.last_name) 'name' FROM employee e JOIN employee m ON (m.manager_id = e.id) WHERE m.manager_id IS NOT NULL;";
+  connection.query(query, (err, results) => {
+    if (err) throw err;
+    // console.table(results);
+    inquirer
+      .prompt({
+        name: "choice",
+        type: "list",
+        choices() {
+          const choiceArray = [];
+          results.forEach(({ name }) => {
+            choiceArray.push({ name });
+          });
+          //   console.log(choiceArray);
+          return choiceArray;
+        },
+        message: "Which Manager",
+      })
+      .then((answer) => {
+        connection.query(
+          "SELECT e.id, e.first_name, e.last_name, r.title, d.name 'department', r.salary, concat(m.first_name, ' ',m.last_name) 'Manager' FROM employee e LEFT JOIN employee m ON (e.manager_id = m.id) JOIN role r on e.role_id=r.id JOIN department d on r.department_id=d.id WHERE (SELECT DISTINCT x.id from employee x WHERE concat(x.first_name,' ',x.last_name) = ?) = m.id;",
+          [answer.choice],
+          (err, res) => {
+            console.table(res);
+            start();
+          }
+        );
+      });
+  });
+};
 
 async function addEmployee() {
   await console.log("addEmployee");
