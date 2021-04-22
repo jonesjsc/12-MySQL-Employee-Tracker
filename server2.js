7d27618b0db8d2496bd1f7c8585e133781492ad8
Jason Jones <jason.e.jones@gmail.com>
2021-04-21 22:58:42 -0400

    the struggle is real

--- server2.js
+++ server2.js
@@ -2,7 +2,7 @@ const mysql = require("mysql");
 const inquirer = require("inquirer");
 const util = require("util");
 const cTable = require("console.table");
-const express = require("express");
+// const express = require("express");
 require("dotenv").config();
 var role_to_add = "";
 var fn_to_add = "";
@@ -197,109 +197,139 @@ const viewEmpByMgr = () => {
   });
 };
 
-const addEmployee = () => {
+const addEmployee = async () => {
   // prompt for info about the item being put up for auction
-  const query = "SELECT title from role;";
-  connection.query(query, (err, results) => {
-    if (err) throw err;
-    inquirer
-      .prompt([
-        {
-          name: "first_name",
-          type: "input",
-          message: "What is the employee's first name?",
-        },
-        {
-          name: "last_name",
-          type: "input",
-          message: "What is the employee's last name?",
-        },
-        {
-          name: "title",
-          type: "list",
-          message: "What is the employee's role?",
-          choices() {
-            //     return roles;
-            //   },
-            // choices() {
-            const choiceArray = [];
-            results.forEach(({ title }) => {
-              choiceArray.push({ title });
-            });
-            const titles = choiceArray.map((role) => {
-              return role.title;
-            });
-            return titles;
-          },
-        },
-      ])
-      //         choices() {
-      //           connection.query("SELECT e.id, e.first_name, e.last_name, r.title, d.name 'department', r.salary, concat(m.first_name, ' ',m.last_name) 'Manager' FROM employee e LEFT JOIN employee m ON (e.manager_id = m.id) JOIN role r on e.role_id=r.id JOIN department d on r.department_id=d.id WHERE (SELECT DISTINCT x.id from employee x WHERE concat(x.first_name,' ',x.last_name) = ?) = m.id;",
-      //           [answer.title],
-      //           (err, results) => {
-      //               console.table(results);
-      //             const choiceArray = [];
-      //             results.forEach(({ title }) => {
-      //               choiceArray.push({ title });
-      //             });
-      //             const titles = choiceArray.map((role) => {
-      //               return role.title;
-      //             });
-      //             return titles;
-      //           },
-      //    )}
-      .then((answer) => {
-        fn_to_add = answer.first_name;
-        ln_to_add = answer.last_name;
-        role_to_add = answer.title;
-      })
-      .then((answer) => {
-        const query =
-          "SELECT DISTINCT e.id, concat(e.first_name, ' ',e.last_name) 'name' FROM employee e JOIN employee m ON (m.manager_id = e.id) WHERE m.manager_id IS NOT NULL;";
-        connection.query(query, (err, results) => {
-          if (err) throw err;
-          console.table(results);
-          inquirer.prompt({
-            name: "manager",
-            type: "list",
-            message: "What is the Managers Name?",
-            choices() {
-              const choiceArray = [];
-              results.forEach(({ name }) => {
-                choiceArray.push({ name });
-              });
-              return choiceArray;
-            },
-          });
+  const query2 = "SELECT title from role;";
+  const query3 =
+    "SELECT DISTINCT e.id, concat(e.first_name, ' ',e.last_name) 'name' FROM employee e JOIN employee m ON (m.manager_id = e.id) WHERE m.manager_id IS NOT NULL;";
+  const roleData = await query(query2);
+  console.table(roleData);
+  console.log(typeof roleData);
+  console.log("xoxoxo");
+  console.log("row 2 " + roleData[2].title);
+
+  const managerData = await query(query3);
+  console.table(managerData);
+
+  const inquirerPrompt = await inquirer.prompt([
+    {
+      name: "first_name",
+      type: "input",
+      message: "What is the employee's first name?",
+    },
+    {
+      name: "last_name",
+      type: "input",
+      message: "What is the employee's last name?",
+    },
+    {
+      name: "title",
+      type: "list",
+      message: "What is the employee's role?",
+      choices() {
+        const choiceArray = [];
+        roleData.forEach(({ title }) => {
+          choiceArray.push({ title });
         });
-      })
-      .then((answer) => {
-        console.log("answer is " + answer);
-        console.table(answer);
-        // manager_to_add = answer.name;
-        console.log("first name " + fn_to_add);
-        console.log("last name " + ln_to_add);
-        console.log("role to add " + role_to_add);
-        console.log("manager name " + manager_to_add);
-        // connection.query(
-        //   "INSERT INTO auctions SET ?",
-        //   // QUESTION: What does the || 0 do?
-        //   {
-        //     item_name: answer.item,
-        //     category: answer.category,
-        //     starting_bid: answer.startingBid || 0,
-        //     highest_bid: answer.startingBid || 0,
-        //   },
-        //   (err) => {
-        //     if (err) throw err;
-        //     console.log("Your auction was created successfully!");
-        //     // re-prompt the user for if they want to bid or post
-        //     start();
-        //   }
-        // );
-      });
-  });
+        return choiceArray;
+      },
+
+      name: "manager",
+      type: "list",
+      message: "Who is the Manager?",
+      choices() {
+        const choiceArray = [];
+        managerData.forEach(({ mgr }) => {
+          choiceArray.push({ mgr });
+        });
+        return choiceArray;
+      },
+    },
+  ]);
+
+  //       choices() {
+  //         //     return roles;
+  //         //   },
+  //         // choices() {
+  //         const choiceArray = [];
+  //         results.forEach(({ title }) => {
+  //           choiceArray.push({ title });
+  //         });
+  //         const titles = choiceArray.map((role) => {
+  //           return role.title;
+  //         });
+  //         return titles;
+  //       },
+  //     },
+  //   ])
+  //         choices() {
+  //           connection.query("SELECT e.id, e.first_name, e.last_name, r.title, d.name 'department', r.salary, concat(m.first_name, ' ',m.last_name) 'Manager' FROM employee e LEFT JOIN employee m ON (e.manager_id = m.id) JOIN role r on e.role_id=r.id JOIN department d on r.department_id=d.id WHERE (SELECT DISTINCT x.id from employee x WHERE concat(x.first_name,' ',x.last_name) = ?) = m.id;",
+  //           [answer.title],
+  //           (err, results) => {
+  //               console.table(results);
+  //             const choiceArray = [];
+  //             results.forEach(({ title }) => {
+  //               choiceArray.push({ title });
+  //             });
+  //             const titles = choiceArray.map((role) => {
+  //               return role.title;
+  //             });
+  //             return titles;
+  //           },
+  //    )}
+  // .then((answer) => {
+  //   fn_to_add = answer.first_name;
+  //   ln_to_add = answer.last_name;
+  //   role_to_add = answer.title;
+  //   const query =
+  //     "SELECT DISTINCT e.id, concat(e.first_name, ' ',e.last_name) 'name' FROM employee e JOIN employee m ON (m.manager_id = e.id) WHERE m.manager_id IS NOT NULL;";
+  //   connection.query(query, (err, results) => {
+  //     if (err) throw err;
+  //     console.table(results);
+  //     inquirer.prompt({
+  //       name: "manager",
+  //       type: "list",
+  //       message: "What is the Managers Name?",
+  //       choices() {
+  //         const choiceArray = [];
+  //         results.forEach(({ name }) => {
+  //           choiceArray.push({ name });
+  //         });
+  //         return choiceArray;
+  //       },
+  //     }).then(data => {
+  //       console.log(data)
+  //     })
+  //   });
+  // })
+  // .then((answer) => {
+  //   console.log("answer is " + answer);
+  //   console.table(answer);
+  //   // manager_to_add = answer.name;
+  //   console.log("first name " + fn_to_add);
+  //   console.log("last name " + ln_to_add);
+  //   console.log("role to add " + role_to_add);
+  //   console.log("manager name " + manager_to_add);
+  //   // connection.query(
+  //   //   "INSERT INTO auctions SET ?",
+  //   //   // QUESTION: What does the || 0 do?
+  //   //   {
+  //   //     item_name: answer.item,
+  //   //     category: answer.category,
+  //   //     starting_bid: answer.startingBid || 0,
+  //   //     highest_bid: answer.startingBid || 0,
+  //   //   },
+  //   //   (err) => {
+  //   //     if (err) throw err;
+  //   //     console.log("Your auction was created successfully!");
+  //   //     // re-prompt the user for if they want to bid or post
+  //   //     start();
+  //   //   }
+  //   // );
+  // });
+  // });
 };
+
 // async function addEmployee() {
 //   await console.log("addEmployee");
 //   start();
